14b076f92b04675c709cbf246c29424a3e42247f
saguero <sergiogaguero@gmail.com>
2021-12-09 20:43:08 -0300

    add onblur and on paste validation

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useEffect  } from 'react'
+import { useNavigate } from 'react-router-dom';
 import HeaderBg from '../../components/layouts/HeaderBg'
 import './Home.scss';
 
@@ -8,7 +9,11 @@ interface Props {
 
 const Home = (props: Props) => {
 
-    const ERROR_MESSAGE_EMAIL = 'Please enter a valid email!';
+    const ERROR_MESSAGE: any = {
+        EMAIL: 'Please enter a valid email!',
+        PHONE: 'Please enter a valid phone number!'
+    };
+
     enum LOGIN_TYPE  {
         EMAIL,
         PHONE
@@ -18,8 +23,9 @@ const Home = (props: Props) => {
     const [errorMessage, setErrorMessage] = useState<string | null>('');
     const [typeLogin, setTypeLogin] = useState<any | null>(LOGIN_TYPE.EMAIL);
     const [buttonDisabled, setButtonDisabled] = useState<boolean | null>(false);
-    const [inputSt, setInput] = useState<any | null>('');
+    const [inputSt, setInput] = useState<string>('');
     const emailRegex = /\S+@\S+\.\S+/;
+    const phoneRegex = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
 
     const onChangeInput= (e: any) => {
         const input = e.target.value;
@@ -32,40 +38,35 @@ const Home = (props: Props) => {
     const checkTypeValidation = (type: number) => {
         switch (type) {
             case LOGIN_TYPE.EMAIL:
-                validateEmail(inputSt);
+                emailRegex.test(inputSt) ? setIsValid(true) : setIsValid(false) 
                 break;
             case LOGIN_TYPE.PHONE:
-                validatePhone(inputSt);
-                break;
-            default:
+                phoneRegex.test(inputSt) ? setIsValid(true) : setIsValid(false) 
                 break;
         }
-    }
 
-    const validateEmail = (email: string | null)=> {
-        if (email && emailRegex.test(email)) {
-            setIsValid(true);
-        } else {
-            setIsValid(false);
-            setErrorMessage(ERROR_MESSAGE_EMAIL);
+        if( !isValid) {
+            setErrorMessage( ERROR_MESSAGE[LOGIN_TYPE[type]] );
         }
-    }
-
-    const validatePhone = (phone: string | null)=> {
-            setIsValid(true);
-    }
+    }  
 
     const clickOption = (type: number)=> {
         setErrorMessage('');
+        setInput('');
         setTypeLogin(type);
         checkTypeValidation(type);
     }
 
     useEffect(()=>{
-        setInput('e.currentTarget.value');
+        setInput('');
         setButtonDisabled(true);
     }, [])
 
+    const navigate = useNavigate();
+
+    const clickContinue = ()=> {
+        navigate('/verification');
+    }
 
     return (
         <main>
@@ -76,9 +77,9 @@ const Home = (props: Props) => {
                     <a className={ (typeLogin === LOGIN_TYPE.EMAIL ? ' --btn-active' : '') + " home__selectors__button"} onClick={()=> { clickOption(LOGIN_TYPE.EMAIL) }}>Email</a>
                     <a className={ (typeLogin === LOGIN_TYPE.PHONE ? ' --btn-active' : '') + " home__selectors__button"} onClick={()=> { clickOption(LOGIN_TYPE.PHONE)  }}>Phone</a>
                 </div>
-                <input type="text"  ref={setInput } onChange={(onChangeInput)} placeholder={typeLogin === LOGIN_TYPE.PHONE ? "Ex. (373) 378 8383" : "jhondoe@gmail.com"} className="home__selectors__input" />
+                <input type="text" value={inputSt} onPaste={onChangeInput} onBlur={onChangeInput} onChange={onChangeInput} placeholder={typeLogin === LOGIN_TYPE.PHONE ? "Ex. (373) 378 8383" : "jhondoe@gmail.com"} className="home__selectors__input" />
                 { !isValid ? ( <p className='error-text'> {errorMessage}</p> ) : ''}
-                <button disabled={ buttonDisabled || !isValid } className="button home__button" >Continue</button>
+                <button disabled={ buttonDisabled || !isValid } className="button home__button" onClick={clickContinue} >Continue</button>
                 <p>by clicking continue you must agree to near labs <a> Terms & Conditions</a>  ans <a> Privacy Policy</a></p>
                 <span className="home__question"> Already have NEAR account?</span>
                 <button className="button btn-dark home__button" >Login with NEAR</button>
