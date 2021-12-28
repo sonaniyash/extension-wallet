09cc9c1431f60befbc8f615a19cc4788fdd7d6e7
saguero <sergiogaguero@gmail.com>
2021-12-28 15:51:17 -0300

    Contact: Create Contact / Import Contacts Modal

---
+++ src/styles/variables.tsx
@@ -0,0 +1,22 @@
+export const VARIABLES: any = {
+    $white: "#ffffff",
+    $grey_2: "#828282",
+    $red: "#dd0d0d",
+    $grey_light_1: "#f2f2f2",
+    $grey_light_2: "#bebec2",
+    $grey_light_3: "#dfdfe0",
+    $light_blue_1: "#eaefff",
+    $light_purple: "#f7e9ff",
+    $accent: "#b072ff",
+    $accent_2: "#885fff",
+    $accent_3: "#3bd0ac",
+    $black: "#414047",
+    $blue_100: "#587be0",
+    $text_100: "#414047",
+    $text_200: "#6f6e73",
+    $text_400: "#a0a0a2",
+    $line_color: "#e9e6e6"
+};
+
+
+
