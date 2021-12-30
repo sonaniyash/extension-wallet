fd216e3f60d448501a84798ee8335c98d0c4da7a
Federico <federicotrezza94@gmail.com>
2021-12-30 18:24:50 -0300

    Added Connected tab in contact

--- src/views/DetailContact/index.tsx
+++ src/views/DetailContact/index.tsx
@@ -2,69 +2,86 @@ import React, { useEffect, useRef, useState } from "react";
 import Modal from "react-modal";
 import { useParams } from "react-router-dom";
 
+import { ContactIcon, DetailSection, EditContact, HeaderContact, SeeTransContact, SubtitleEmail, NameH2, ConnectedCount } from "./styles";
 import { Contact } from "../../components/ContactItem";
-import { TEST_CONTACTS } from "../../mock/mock";
+import { TEST_CONTACTS, TEST_EXPERIENCES } from "../../mock/mock";
 import TabsHeader from "../../components/common/TabsHeader";
 import TabsContainer from "../../components/common/TabsContainer";
 import CollectibleItem from "../../components/common/CollectibleItem";
 import HeaderAccountSelect from "../../components/common/HeaderAccountSelect";
 
-import { ContactIcon, DetailSection, EditContact, HeaderContact, SeeTransContact, SubtitleEmail, NameH2 } from "./styles";
+import ConnectedExpItem, { ConnectedExp } from "../../components/ConnectedExpItem";
 
 Modal.setAppElement("#popup");
 
 const DetailContacts = () => {
 
   const [contact, setcontact] = useState<Contact | null>(TEST_CONTACTS[0])
+  const [exps, setExps] = useState<ConnectedExp[]>()
   const [activeTab, setActive] = useState(0);
   const tab1 = useRef<any>();
   const tab2 = useRef<any>();
+  const tab3 = useRef<any>();
 
-  const getContactData = (id: string): Contact | null  => {
-    const contact = TEST_CONTACTS.find( (val: Contact)=> val.id === id );
+  const getContactData = (id: string): Contact | null => {
+    const contact = TEST_CONTACTS.find((val: Contact) => val.id === id);
     return contact ? contact : null;
+  }
+  const getConnectedExpData = (id: string): ConnectedExp[] => {
+    const exps = TEST_EXPERIENCES.filter((exp: ConnectedExp) => exp.relatedAccounts?.find((coso) => id == coso.toString()));
+    return exps ? exps : [];
   }
   const { id } = useParams();
 
   useEffect(() => {
     if (id) {
       setcontact(getContactData(id));
+      setExps(getConnectedExpData(id));
     }
   }, [id])
 
   return (
     <>
-      <HeaderAccountSelect/>
+      <HeaderAccountSelect />
       <DetailSection>
         <HeaderContact>
           <ContactIcon>
-            { contact? contact?.firstName?.substring(0, 1)?.toUpperCase() + contact?.lastName?.substring(0, 1)?.toUpperCase() : ''}
+            {contact ? contact?.firstName?.substring(0, 1)?.toUpperCase() + contact?.lastName?.substring(0, 1)?.toUpperCase() : ''}
           </ContactIcon>
           <div>
             <SeeTransContact></SeeTransContact>
             <EditContact></EditContact>
           </div>
         </HeaderContact>
-        <NameH2> { contact ?  `${contact.firstName} ${contact.lastName}` : '' }</NameH2>
-        <SubtitleEmail> { contact ? contact.account : ''}</SubtitleEmail>
+        <NameH2> {contact ? `${contact.firstName} ${contact.lastName}` : ''}</NameH2>
+        <SubtitleEmail> {contact ? contact.account : ''}</SubtitleEmail>
       </DetailSection>
       <TabsHeader
-          tabsHeader={["Collectibles", "Actions", "Connected expereinces"]}
-          setActive={setActive}
-        />
-      <TabsContainer tabs={[tab1, tab2]} activeTabId={activeTab}>
-          <>
-            <div data-tab="0" ref={tab1} className="tab-text">
-              <CollectibleItem item={0} />
-              <CollectibleItem item={0} />
-              <CollectibleItem item={0} />
-              <CollectibleItem item={0} />
-            </div>
-            <div data-tab="1" ref={tab2} className="tab-text">
-              text tab 2
-            </div>
-          </>
-        </TabsContainer>
+        tabsHeader={["Collectibles", "Actions", "Connected expereinces"]}
+        setActive={setActive}
+      />
+      <TabsContainer tabs={[tab1, tab2, tab3]} activeTabId={activeTab}>
+        <>
+          <div data-tab="0" ref={tab1} className="tab-text">
+            <CollectibleItem item={0} />
+            <CollectibleItem item={0} />
+            <CollectibleItem item={0} />
+            <CollectibleItem item={0} />
+          </div>
+          <div data-tab="1" ref={tab2} className="tab-text">
+            text tab 2
+          </div>
+          <div data-tab="2" ref={tab3} className="tab-text">
+            {exps && exps?.length > 0 && (
+              <ConnectedCount>
+                {exps?.length} connected experiences with devon
+              </ConnectedCount>)}
+            {exps ? exps.map((exp) => (
+              <ConnectedExpItem item={exp} />
+            )) : null}
+          </div>
+        </>
+      </TabsContainer>
     </>
   );
 };
