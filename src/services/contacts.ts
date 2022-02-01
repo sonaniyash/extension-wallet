90d5347a79aee2f0177bad88226aa5447aa01dd9
rohit9210 <97750710+rohit9210@users.noreply.github.com>
2022-02-01 20:17:00 +0530

    added No wallet associated feature in contact detail screen 🙂 (#61)
    * added No wallet associated feature in contact detail screen 🙂
    
    * hide the transaction button for un associated wallet
    
    * removed mock datd from contact api call, replaced with API call
    
    * EX-66 UI fixes
    
    Co-authored-by: rohit <roit@primelab.io>
    Co-authored-by: Aleksandr Ufimtsev <fimak@bk.ru>

--- src/services/contacts.ts
+++ src/services/contacts.ts
@@ -1,7 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
+
 export const apiNearUrl = `${process.env.EXTENSION_ENDPOINT}`;
-
-
 
 const api = {
   getContacts: async (userId: any): Promise<Array<any>> => {
@@ -14,52 +13,46 @@ const api = {
     return axios
       .get(`${apiNearUrl}/contacts/${id}`)
       .then((response: any) => response.data.data);
-    // return new Promise(function (resolve) {
-    //   return setTimeout(() => {
-    //     const result = TEST_CONTACTS.find((val)=> val.id ===id)
-    //     resolve(result)
-    //   }, 700);
-      // return setTimeout(() => reject(), 1500);
-    // });
   },
+
   editContact: async (contactData: any, userId: any, id: any) => {
     const payload = {
       first_name: contactData.first_name,
       email: [
         {
           address: contactData.email,
-          type: "home"
+          type: 'home'
         }
       ],
       phone: [
-          {
-            number: contactData && contactData.phone ? contactData.phone : '',
-            type: "home"
-          }
+        {
+          number: contactData && contactData.phone ? contactData.phone : '',
+          type: 'home'
+        }
       ],
-          last_name: contactData.last_name,
-          address: [
-            {
-              formatted: contactData.address,
-              type: "residence"
-            }
-          ],
-          job_title: "",
-          companies: [],
-          groups: [],
-          dob: "",
-          wallet_id: contactData.nearAccount,
-          birthday: "",
-          import_source: "",
-          app_id: "",
-          owner_id: userId
-      }
-     return axios
-       .put(`${apiNearUrl}/contacts/${id}`, payload)
-       .then((response) => response.data);
+      last_name: contactData.last_name,
+      address: [
+        {
+          formatted: contactData.address,
+          type: 'residence'
+        }
+      ],
+      job_title: '',
+      companies: [],
+      groups: [],
+      dob: '',
+      wallet_id: contactData.nearAccount,
+      birthday: '',
+      import_source: '',
+      app_id: '',
+      owner_id: userId
+    };
+    return axios
+      .put(`${apiNearUrl}/contacts/${id}`, payload)
+      .then((response) => response.data);
     // return new Promise(function (resolve) {
     //   return setTimeout(() => resolve(contactData), 1500);
-      // return setTimeout(() => reject(), 1500);
+    // return setTimeout(() => reject(), 1500);
     // });
   },
   createContact: async (contactData: any, userId: any) => {
@@ -67,37 +60,37 @@ const api = {
       first_name: contactData.first_name,
       email: [{
         address: contactData.email,
-        type: "home"
+        type: 'home'
       }],
       phone: [
-          {
-            number: contactData && contactData.phone ? contactData.phone : '',
-            type: "home"
-          }
-  ],
+        {
+          number: contactData && contactData.phone ? contactData.phone : '',
+          type: 'home'
+        }
+      ],
       last_name: contactData.last_name,
       address: [
         {
           formatted: contactData.address,
-          type: "residence"
+          type: 'residence'
         }
       ],
-      job_title: "",
+      job_title: '',
       companies: [],
       groups: [],
-      dob: "",
+      dob: '',
       wallet_id: contactData.nearAccount,
-      birthday: "",
-      import_source: "",
-      app_id: "HomePage Extension App",
+      birthday: '',
+      import_source: '',
+      app_id: 'HomePage Extension App',
       owner_id: userId
-  }
+    };
     return axios
       .post(`${apiNearUrl}/contacts`, payload)
       .then((response) => response.data);
     // return new Promise(function (resolve) {
-      // return setTimeout(() => resolve(contactData), 1500);
-      // return setTimeout(() => reject(), 1500);
+    // return setTimeout(() => resolve(contactData), 1500);
+    // return setTimeout(() => reject(), 1500);
     // });
   },
 };
--- src/views/DetailContact/index.tsx
+++ src/views/DetailContact/index.tsx
@@ -1,28 +1,39 @@
-import React, { useRef, useState } from "react";
-import Modal from "react-modal";
-import { useNavigate, useParams } from "react-router-dom";
+import React, { useRef, useState } from 'react';
+import Modal from 'react-modal';
+import { useNavigate, useParams } from 'react-router-dom';
 
-import TabsHeader from "../../components/common/TabsHeader";
-import TabsContainer from "../../components/common/TabsContainer";
-import CollectibleItem from "../../components/common/CollectibleItem";
-import HeaderAccountSelect from "../../components/common/HeaderAccountSelect";
+import TabsHeader from '../../components/common/TabsHeader';
+import TabsContainer from '../../components/common/TabsContainer';
+import CollectibleItem from '../../components/common/CollectibleItem';
+import HeaderAccountSelect from '../../components/common/HeaderAccountSelect';
 
-import ConnectedExpItem, { ConnectedExp } from "../../components/ConnectedExpItem";
+import ConnectedExpItem, { ConnectedExp } from '../../components/ConnectedExpItem';
 
-import { ContactIcon, DetailSection, EditContact, HeaderContact, SeeTransContact, SubtitleEmail, NameH2, ConnectedCount, ButtonWrapper } from "./styles";
-import { ROUTES } from "../../const/routeNames";
-import arrowUp from "../../public/assets/arrow-up.png";
-import arrowDown from "../../public/assets/arrow-down.png";
-import SendModal from "../../components/SendModal";
-import { useGetContact } from "../../hooks/api/contacts";
+import {
+  ContactIcon,
+  DetailSection,
+  EditContact,
+  HeaderContact,
+  SeeTransContact,
+  SubtitleEmail,
+  NameH2,
+  ConnectedCount,
+  ButtonWrapper,
+  GuestContactWrapper
+} from './styles';
+import { ROUTES } from '../../const/routeNames';
+import arrowUp from '../../public/assets/arrow-up.png';
+import arrowDown from '../../public/assets/arrow-down.png';
+import SendModal from '../../components/SendModal';
+import { useGetContact } from '../../hooks/api/contacts';
 
-Modal.setAppElement("#popup");
+Modal.setAppElement('#popup');
 
 const DetailContacts = () => {
   const { id } = useParams();
-  const {contact} = useGetContact(id);
+  const { contact } = useGetContact(id);
 
-  const [exps] = useState<ConnectedExp[]>()
+  const [exps] = useState<ConnectedExp[]>();
   const [sendIsOpen, setSendIsOpen] = React.useState(false);
   const [receiveIsOpen, setReceiveIsOpen] = React.useState(false);
   const [activeTab, setActive] = useState(0);
@@ -36,11 +47,11 @@ const DetailContacts = () => {
     if (id) {
       nav(ROUTES.EDIT_CONTACT.url.replace(':id', id));
     }
-  }
+  };
 
   const customStyles = {
     overlay: {
-      backgroundColor: "rgba(51, 55, 59, 0.4)",
+      backgroundColor: 'rgba(51, 55, 59, 0.4)',
     },
   };
 
@@ -59,86 +70,124 @@ const DetailContacts = () => {
             {contact ? contact?.first_name?.substring(0, 1)?.toUpperCase() + contact?.last_name?.substring(0, 1)?.toUpperCase() : ''}
           </ContactIcon>
           <div>
-            <SeeTransContact></SeeTransContact>
-            <EditContact onClick={editContact} ></EditContact>
+            {contact && contact.wallet_id ? <SeeTransContact /> : null}
+
+            <EditContact onClick={editContact} />
           </div>
         </HeaderContact>
         <NameH2> {contact ? `${contact.first_name} ${contact.last_name}` : ''}</NameH2>
         <SubtitleEmail> {contact ? contact.wallet_id : ''}</SubtitleEmail>
       </DetailSection>
-      <TabsHeader
-        tabsHeader={["Collectibles", "Actions", "Connected expereinces"]}
-        setActive={setActive}
-      />
-      <TabsContainer tabs={[tab1, tab2, tab3]} activeTabId={activeTab}>
-        <>
-          <div data-tab="0" ref={tab1} className="tab-text">
-            <CollectibleItem item={0} />
-            <CollectibleItem item={0} />
-            <CollectibleItem item={0} />
-            <CollectibleItem item={0} />
-          </div>
-          <div data-tab="1" ref={tab2} className="tab-text">
-            <ButtonWrapper onClick={() => {
+
+      {contact && contact.wallet_id ?
+        <div>
+          <TabsHeader
+            tabsHeader={['Collectibles', 'Actions', 'Connected expereinces']}
+            setActive={setActive}
+            activeTab={activeTab}
+          />
+          <TabsContainer tabs={[tab1, tab2, tab3]} activeTabId={activeTab}>
+            <>
+              <div data-tab="0" ref={tab1} className="tab-text">
+                <CollectibleItem item={0} />
+                <CollectibleItem item={0} />
+                <CollectibleItem item={0} />
+                <CollectibleItem item={0} />
+              </div>
+              <div data-tab="1" ref={tab2} className="tab-text">
+                <ButtonWrapper onClick={() => {
+                  setSendIsOpen(true);
+                }}>
+                  <div className="body">
+                  <span className="body__title">
+                    Send
+                  </span>
+                  </div>
+                  <img
+                    className="icon"
+                    src={arrowUp}
+                    alt=""
+                  />
+                </ButtonWrapper>
+                <ButtonWrapper onClick={() => {
+                  setReceiveIsOpen(true);
+                }}>
+                  <div className="body">
+                  <span className="body__title">
+                    Receive
+                  </span>
+                  </div>
+                  <img
+                    className="icon"
+                    src={arrowDown}
+                    alt=""
+                  />
+                </ButtonWrapper>
+                <div>
+                  <Modal
+                    id="customModal"
+                    isOpen={sendIsOpen}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                    className="open-modal"
+                  >
+                    <SendModal entity="send" id={id ? id : ''} />
+                  </Modal>
+                  <Modal
+                    id="customModal"
+                    isOpen={receiveIsOpen}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                    className="open-modal"
+                  >
+                    <SendModal entity="receive" id={id ? id : ''} />
+                  </Modal>
+                </div>
+              </div>
+              <div data-tab="2" ref={tab3} className="tab-text">
+                {exps && exps?.length > 0 && (
+                  <ConnectedCount>
+                    {exps?.length} connected experiences with devon
+                  </ConnectedCount>)}
+                {exps ? exps.map((exp) => (
+                  <ConnectedExpItem item={exp} />
+                )) : null}
+              </div>
+            </>
+          </TabsContainer>
+        </div> :
+        <GuestContactWrapper>
+          <ButtonWrapper
+            onClick={() => {
               setSendIsOpen(true);
             }}>
-              <div className="body">
-                <span className="body__title">
-                  Send
-                </span>
-              </div>
-              <img
-                className="icon"
-                src={arrowUp}
-                alt=""
-              />
-            </ButtonWrapper>
-            <ButtonWrapper onClick={() => {
-              setReceiveIsOpen(true)
-            }}>
-              <div className="body">
-                <span className="body__title">
-                  Receive
-                </span>
-              </div>
-              <img
-                className="icon"
-                src={arrowDown}
-                alt=""
-              />
-            </ButtonWrapper>
-            <div>
-              <Modal
-                id="customModal"
-                isOpen={sendIsOpen}
-                onRequestClose={closeModal}
-                style={customStyles}
-                className="open-modal"
-              >
-                <SendModal entity="send" id={id ? id : ''} />
-              </Modal>
-              <Modal
-                id="customModal"
-                isOpen={receiveIsOpen}
-                onRequestClose={closeModal}
-                style={customStyles}
-                className="open-modal"
-              >
-                <SendModal entity="receive" id={id ? id : ''}/>
-              </Modal>
+            <div className="body">
+            <span className="body__title">
+              Share NFT
+            </span>
             </div>
-          </div>
-          <div data-tab="2" ref={tab3} className="tab-text">
-            {exps && exps?.length > 0 && (
-              <ConnectedCount>
-                {exps?.length} connected experiences with devon
-              </ConnectedCount>)}
-            {exps ? exps.map((exp) => (
-              <ConnectedExpItem item={exp} />
-            )) : null}
-          </div>
-        </>
-      </TabsContainer>
+            <img
+              className="icon"
+              src={arrowUp}
+              alt=""
+            />
+          </ButtonWrapper>
+          <ButtonWrapper onClick={() => {
+            setReceiveIsOpen(true);
+          }}>
+            <div className="body">
+            <span className="body__title">
+              Invite contact
+            </span>
+            </div>
+            <img
+              className="icon"
+              src={arrowDown}
+              alt=""
+            />
+          </ButtonWrapper>
+        </GuestContactWrapper>}
+
     </>
   );
 };
