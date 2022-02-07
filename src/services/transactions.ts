989f75baf84e97f94c8b29fe311336fe0069185b
Aleksandr Ufimtsev <fimak@bk.ru>
2022-02-07 19:31:04 +0600

    EX-54 switched to real data

--- src/services/transactions.ts
+++ src/services/transactions.ts
@@ -1,13 +1,13 @@
-// import axios from "axios";
-// export const apiNearUrl = `${process.env.EXTENSION_ENDPOINT}`;
-import TRANSACTIONS from '../mock/transactions';
+import axios from "axios";
+export const apiNearUrl = `${process.env.EXTENSION_ENDPOINT}`;
+// import TRANSACTIONS from '../mock/transactions';
 
 const api = {
   getTransactions: async (userId: string): Promise<Array<any>> => {
-    // return axios
-    //   .get(`${apiNearUrl}/transactions/list/${userId}`)
-    //   .then((response) => response.data.data);
-    return new Promise((resolve) => setTimeout(() => resolve(TRANSACTIONS.data), 700));
+    return axios
+      .get(`${apiNearUrl}/transactions/list/${userId}`)
+      .then((response) => response.data.data);
+    // return new Promise((resolve) => setTimeout(() => resolve(TRANSACTIONS.data), 700));
   },
 };
 
